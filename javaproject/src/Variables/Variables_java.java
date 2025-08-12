35638bec26e2e687cba813e7e32200abc29a45e9
c6454 <c6454@SPANDANA>
2025-08-12 10:15:18 +0530

    first commit

---
+++ javaproject/src/Variables/Variables_java.java
@@ -0,0 +1,23 @@
+package Variables;
+
+public class Variables_java {
+
+		 int a = 10; // Instance variable
+
+		    void print() {
+		        String msg = "hello";
+		        System.out.println(msg);
+		    }
+
+		    static String message = "hello students"; // Static variable
+
+		    public static void main(String[] args) {
+		    	Variables_java  obj = new Variables_java ();
+		        System.out.println("The value of a: " + obj.a);
+		        obj.print();
+		        System.out.println(message);
+	}
+
+}
+
+
